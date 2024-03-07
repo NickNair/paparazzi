f62cc2999a95c2c6713a44cacfdbd51255043bbf
Nikhil Nair <nikhilnicky972@gmail.com>
2024-03-07 19:44:22 +0100

    Added
     - New airframe bebop_group_5: Just a copy of orange avoid with the algorithm removed
     - stream_video.py to stream video for testing cv algorithms in sim and for the drone

---
+++ Group5/stream_video.py
@@ -0,0 +1,73 @@
+#! /usr/bin/python
+
+import cv2
+import sys
+import argparse
+from os import path, getenv
+import os
+
+# See the issue and solution here: https://github.com/opencv/opencv/issues/10328
+os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'protocol_whitelist;file,rtp,udp'
+
+class RtpViewer:
+    running = False
+    scale = 1
+    rotate = 1
+    frame = None
+    mouse = dict()
+
+    def __init__(self, src):
+
+        # Create the video capture device
+        self.cap = cv2.VideoCapture(src)
+        cv2.namedWindow('rtp')
+
+    def run(self):
+        self.running = True
+
+        # Start an 'infinite' loop
+        while self.running:
+            # Read a frame from the video capture
+            ret, self.frame = self.cap.read()
+
+            # Quit if frame could not be retrieved
+            if not ret:
+                break
+
+            # Run the computer vision function
+            self.cv()
+
+            # Process key input
+            self.on_key(cv2.waitKey(1) & 0xFF)
+
+    def cv(self):
+        # Rotate the image by increments of 90
+        self.frame = cv2.transpose(self.frame)
+
+        self.frame = cv2.flip(self.frame, 0)
+
+        # Show the image in a window
+        cv2.imshow('rtp', self.frame)
+
+    def on_key(self, key):
+        if key == ord('q'):
+            self.running = False
+
+        if key == ord('r'):
+            self.rotate = (self.rotate + 1) % 4
+            self.mouse['start'] = None
+
+
+
+if __name__ == '__main__':
+
+    # change the path here
+    filename = "/home/nicknair/AE4317/paparazzi/sw/tools/rtp_viewer/rtp_5000.sdp"
+
+    viewer = RtpViewer(filename)
+
+    if not viewer.cap.isOpened():
+        raise IOError("Can't open video stream")
+
+    viewer.run()
+
